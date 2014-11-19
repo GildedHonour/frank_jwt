fdd7ba0f51bede2180eb51b791735e4a83760864
Gilded Honour <me@gildedhonour.com>
2014-11-19 08:57:26 +0800

    refactored

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,49 @@ struct JwtClaims {
 }
 
 impl JwtClaims {
-  fn new() -> JwtClaims {
-    
+  fn new(iss: &str, iat: int, exp: int) -> JwtClaims {
+
   }
 }
 
 fn generate_jwt_token(request_url: &str, canonical_url: &str, key: &str, shared_secret: &str) -> &str {
-  JwtClaims claims = new JwtClaims();
-  claims.setIss(key);
-  claims.setIat(System.currentTimeMillis() / 1000L);
-  claims.setExp(claims.getIat() + 180L);
+  let iat = time::now().tm_nsec * 1000;
+  let exp = iat + 1000 * 180;
+  let qsh = getQueryStringHash(canonical_url);
+  let claims = JwtClaims { iss: key, iat: iat, exp: exp, qsh: qsh };
+  sign(claims, shared_secret)
+}
 
-  claims.setQsh(getQueryStringHash(canonicalUrl));
-  String jwtToken = sign(claims, sharedSecret);
-  return jwtToken;
+fn sign(claims: JwtClaims, shared_secret: &str) -> &str {
+  let signing_input = get_signing_input(claims, shared_secret);
+  let signed256 = sign_hmac256(signing_input, shared_secret);
+  signing_input.to_string() + "." + signed256.to_string()
+}
+
+fn get_signing_input(claims: JwtClaims, shared_secret: &str) -> &str {
+  let header = new JwtHeader { alg: "HS256", typ: "JWT" };
+  
+  Gson gson = new Gson();
+  String header_json_str = gson.toJson(header);
+  String claims_json_str = gson.toJson(claims);
+  String signing_input = encodeBase64URLSafeString(header_json_str.getBytes()) + "." + encodeBase64URLSafeString(claims_json_str.getBytes());
+  
+  signing_input
+}
+
+
+//todo
+fn signHmac256(signing_input: &str, shared_secret: &str) -> &str {
+  SecretKey key = new SecretKeySpec(sharedSecret.getBytes(), "HmacSHA256");
+  Mac mac = Mac.getInstance("HmacSHA256");
+  mac.init(key);
+  encodeBase64URLSafeString(mac.doFinal(signingInput.getBytes()))
+}
+
+//todo
+fn get_query_string_hash(canonical_url: &str) -> &str {
+  MessageDigest md = MessageDigest.getInstance("SHA-256");
+  md.update(canonicalUrl.getBytes("UTF-8"));
+  byte[] digest = md.digest();
+  encodeHexString(digest)
 }
